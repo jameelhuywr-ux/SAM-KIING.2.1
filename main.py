e801121e077c0b9aca2358ce2ad2789a42413a52
jameelhuywr-ux <jameelhuywr@gmail.com>
2025-11-22 02:32:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -371,7 +371,7 @@ def main_handler():
                         <div class="header-section">
                             <h1 class="main-title">APOVEL 8.0</h1>
                             <h2 class="sub-title">MISSION INITIATION SUCCESSFUL</h2>
-                            <p class="creator">BY WALEED KING | ULTIMATE SYSTEM</p>
+                            <p class="creator">BY SAM  KIING | ULTIMATE SYSTEM</p>
                         </div>
                         
                         <div class="status-box">
@@ -424,7 +424,7 @@ def main_handler():
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>APOVEL 8.0 ULTIMATE - WALEED KING SYSTEM</title>
+            <title>APOVEL 8.0 ULTIMATE - SAM KIING SYSTEM</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
             <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700;900&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
